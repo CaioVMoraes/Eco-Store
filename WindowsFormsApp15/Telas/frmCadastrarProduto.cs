6d964a8201f1a6abdd543c498262aa71ff11b0b8
infob04 <infob04@INFO09>
2019-10-07 11:55:28 -0300

    ....

--- WindowsFormsApp15/Telas/frmCadastrarProduto.cs
+++ WindowsFormsApp15/Telas/frmCadastrarProduto.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApp15.Telas
         {
             InitializeComponent();
         }
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
