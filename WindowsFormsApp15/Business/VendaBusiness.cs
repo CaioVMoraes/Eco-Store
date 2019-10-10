8ac6a63342d3172df9b1cb7fa18c5e1db312a60a
infob04 <infob04@INFO09>
2019-10-10 11:51:47 -0300

    Salveeeeee ouuuuu corinnnntiassssssdss o caaampeoiss dos campeoisss

---
+++ WindowsFormsApp15/Business/VendaBusiness.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp15.Model;
+
+namespace WindowsFormsApp15.Business
+{
+    class VendaBusiness
+    {
+        Database.VendaDatabase db = new Database.VendaDatabase();
+
+        public void InserirVenda(tb_venda modelo)
+        {
+            db.InserirVenda(modelo);
+        }
+        public void InserirVendaItem(tb_venda_item modelo)
+        {
+            db.InserirVendaitem(modelo);
+        }
+
+        public List<tb_venda> ConsultarVenda()
+        {
+            List<tb_venda> lista = db.ConsultarVenda();
+
+            return lista;
+        }
+        public tb_venda Listar(int id)
+        {
+            tb_venda modelo = db.Listar(id);
+
+            return modelo;
+        }
+
+        public void AlterarVenda(tb_venda modelo)
+        {
+            db.AlterarVenda(modelo);
+        }
+        public void RemoverVenda(tb_venda modelo)
+        {
+            db.RemoverVenda(modelo);
+        }
+    }
+}
---
+++ WindowsFormsApp15/Database/VendaDatabase.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp15.Model;
+
+namespace WindowsFormsApp15.Database
+{
+    class VendaDatabase
+    {
+        Model.ecostoreEntities db = new Model.ecostoreEntities();
+        public void InserirVenda(tb_venda modelo)
+        {
+            db.tb_venda.Add(modelo);
+            db.SaveChanges();
+        }
+        public void InserirVendaitem(tb_venda_item modelo)
+        {
+            db.tb_venda_item.Add(modelo);
+            db.SaveChanges();
+        } 
+
+        public List<tb_venda> ConsultarVenda()
+        {
+            List<tb_venda> lista = db.tb_venda.ToList();
+
+            return lista;
+        }
+        public tb_venda Listar(int id)
+        {
+            tb_venda modelo = db.tb_venda.FirstOrDefault(x => x.id_venda == id);
+
+            return modelo;
+        }
+
+        public void AlterarVenda(tb_venda modelo)
+        {
+            tb_venda alterar = db.tb_venda.FirstOrDefault(x => x.id_venda == x.id_venda);
+
+            modelo.dt_saida = alterar.dt_saida;
+            modelo.vl_valorTotal = alterar.vl_valorTotal;
+            
+
+            db.SaveChanges();
+        }
+        public void RemoverVenda(tb_venda modelo)
+        {
+            db.tb_venda.Remove(modelo);
+        }
+    }
+}
