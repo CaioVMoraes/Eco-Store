2c1c69169b38fdb7e54f0724c477f957e22131dd
infob04 <infob04@INFO09>
2019-10-15 08:38:56 -0300

    ;;

--- WindowsFormsApp15/Telas/Fluxo de Caixa/frmFluxoCaixa.Designer.cs
+++ WindowsFormsApp15/Telas/Fluxo de Caixa/frmFluxoCaixa.Designer.cs
@@ -98,7 +98,7 @@
             this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(586, 9);
+            this.label1.Location = new System.Drawing.Point(586, 6);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(84, 37);
             this.label1.TabIndex = 56;
