4eff6282d32113574aa62c7ce480d5996c5b1ef5
infob04 <infob04@INFO09>
2019-10-17 10:32:36 -0300

    ;

--- WindowsFormsApp15/Telas/Usuario/frmCadastrarUsuario.cs
+++ WindowsFormsApp15/Telas/Usuario/frmCadastrarUsuario.cs
@@ -17,9 +17,37 @@ namespace WindowsFormsApp15.Telas
             InitializeComponent();
         }
 
+        Model.tb_usuario model = new Model.tb_usuario();
+        Business.UsuarioBusiness business = new Business.UsuarioBusiness();
+
         private void btnEntrar_Click(object sender, EventArgs e)
         {
-            
+            try
+            {
+                //*Informações Pessoais*
+
+                //model.id_funcionario = 
+                model.nm_usuario = txtUsuario.Text;
+                model.
+
+                //*Endereço*
+
+                model.ds_endereco = txtEndereço.Text;
+                model.ds_cep = txtCep.Text;
+                model.ds_cidade = txtCidade.Text;
+                model.ds_UF = cboUF.Text;
+                model.ds_complemento = txtComplemento.Text;
+                model.ds_numeroCasa = txtNumRes.Text;
+                model.img_foto = null;
+
+                business.CadastrarFuncionario(model);
+
+                MessageBox.Show("Cadastrado com Sucesso");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
