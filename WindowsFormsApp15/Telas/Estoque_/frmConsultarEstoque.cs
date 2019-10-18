fbfae2f55626b96426f2a24dab098fda579382af
infob04 <infob04@INFO09>
2019-10-18 10:39:57 -0300

    Estoque

--- WindowsFormsApp15/Telas/Estoque_/frmConsultarEstoque.cs
+++ WindowsFormsApp15/Telas/Estoque_/frmConsultarEstoque.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp15.Model;
 
 namespace WindowsFormsApp15.Telas.Estoque
 {
@@ -19,7 +20,45 @@ namespace WindowsFormsApp15.Telas.Estoque
 
         private void dtpData_onValueChanged(object sender, EventArgs e)
         {
+            Business.EstoqueBusiness business = new Business.EstoqueBusiness();
 
+            DateTime data = dtpData.Value;
+            
+           List<tb_estoque> listar = business.ConsultarEstoqueData(data);
+
+            dgvConsultarEstoque.DataSource = listar;
+        }
+
+        private void txtProduto_TextChanged(object sender, EventArgs e)
+        {
+            Business.EstoqueBusiness business = new Business.EstoqueBusiness();
+
+            int id = Convert.ToInt32(txtProduto.Text);
+
+            List<tb_estoque> listar = business.ConsultarEstoqueId(id);
+
+            dgvConsultarEstoque.DataSource = listar;
+        }
+        private void rdnSim_CheckedChanged(object sender, EventArgs e)
+        {
+            Business.EstoqueBusiness business = new Business.EstoqueBusiness();
+
+            bool vendido = rdnSim.Checked;
+
+            List<tb_estoque> listar = business.ConsultarEstoqueVendidoSim(vendido);
+
+            dgvConsultarEstoque.DataSource = listar;
+        }
+
+        private void rdnNao_CheckedChanged(object sender, EventArgs e)
+        {
+            Business.EstoqueBusiness business = new Business.EstoqueBusiness();
+
+            bool vendido = rdnSim.Checked;
+
+            List<tb_estoque> listar = business.ConsultarEstoqueVendidoNao(vendido);
+
+            dgvConsultarEstoque.DataSource = listar;
         }
     }
-}
+   }
